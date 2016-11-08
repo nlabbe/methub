ae75ff1f8bdfc707a1a7c0e8efc95d6cf0abfb4c
Nicolás Labbé <nlabbea@gmail.com>
2016-11-08 15:00:01 -0300

    Added main survey entiies

---
+++ Models/QuestionDetail.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Methub.Models
+{
+    public class QuestionDetail
+    {
+        public QuestionDetail()
+        {
+        }
+
+        [Key]
+        public int id { get; set; }
+        public int question_id { get; set; }
+        public Guid rowguid { get; set; }
+    }
+}
