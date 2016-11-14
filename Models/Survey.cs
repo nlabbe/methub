9232a9fe1173af8f8f955fec8a84f7a4b48a1757
Nicolás Labbé <nlabbea@gmail.com>
2016-11-14 16:48:44 -0300

    Modified survey entity

--- Models/Survey.cs
+++ Models/Survey.cs
@@ -10,15 +10,20 @@ namespace Methub.Models
     {
         public Survey()
         {
+            // this.date_created = DateTime.Now;
+            // this.date_modified = DateTime.Now;
+            // this.rowguid = Guid.NewGuid();
         }
 
         [Key]
         public int id { get; set; }
+        [RequiredAttribute]
         public string title { get; set; }
+        [RequiredAttribute]
         public string description { get; set; }
-        public DateTime date_created { get; set; }
-        public DateTime date_modified { get; set; }
-        public Guid rowguid { get; set; }
+        public Nullable<DateTime> date_created { get; set; }
+        public Nullable<DateTime> date_modified { get; set; }
+        public Nullable<Guid> rowguid { get; set; }
 
         public List<Question> Questions { get; set; } = new List<Question>();
     }
