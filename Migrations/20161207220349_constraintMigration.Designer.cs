9970ccc06e20caa297b98cf95e9c6fa235d79b87
Nicolás Labbé <nlabbea@gmail.com>
2016-12-17 20:15:25 -0300

    Added constraint migrations

--- Migrations/20161207220349_constraintMigration.Designer.cs
+++ Migrations/20161207220349_constraintMigration.Designer.cs
@@ -8,9 +8,10 @@ using Methub.Data;
 namespace methub.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20161207220349_constraintMigration")]
+    partial class constraintMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.0.1");
---
+++ Migrations/20161207220349_constraintMigration.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace methub.Migrations
+{
+    public partial class constraintMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Constraints",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("Autoincrement", true),
+                    comment = table.Column<string>(nullable: true),
+                    constraint_type = table.Column<int>(nullable: false),
+                    end_date = table.Column<DateTime>(nullable: true),
+                    frequency = table.Column<int>(nullable: false),
+                    max_time = table.Column<DateTime>(nullable: true),
+                    min_time = table.Column<DateTime>(nullable: true),
+                    priority = table.Column<int>(nullable: false),
+                    rol = table.Column<int>(nullable: false),
+                    start_date = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Constraints", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ConstraintDetail",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("Autoincrement", true),
+                    Constraintid = table.Column<int>(nullable: true),
+                    constraint_id = table.Column<int>(nullable: false),
+                    day_id = table.Column<int>(nullable: false),
+                    is_valid_day = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ConstraintDetail", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_ConstraintDetail_Constraints_Constraintid",
+                        column: x => x.Constraintid,
+                        principalTable: "Constraints",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ConstraintDetail_Constraintid",
+                table: "ConstraintDetail",
+                column: "Constraintid");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ConstraintDetail");
+
+            migrationBuilder.DropTable(
+                name: "Constraints");
+        }
+    }
+}
